40f5f72939de054e71c8989bdad820676d3a7ca6
HotJuly <949588380@qq.com>
2021-09-25 16:09:05 +0800

    03.接口

--- 3.TypeScript/02.代码/ts-cli/src/main.ts
+++ 3.TypeScript/02.代码/ts-cli/src/main.ts
@@ -1,74 +1,66 @@
 (function(){
-    let num:number = 123;
+    // 现在有一个对象
+    // id是number类型, 必须有, 只读的
+    // name是string类型, 必须有
+    // age是number类型, 必须有
+    // sex是string类型, 可以没有
 
-    let str:string = "123";
-
-    let flag:boolean = true;
-
-
-    // 在非严格模式下,undefined和null是所有类型的子类
-    // 在严格模式下,undefined和null是自己各自是一家人
-    let un:undefined = undefined;
-
-    let nu:null = null;
-
-    let num1 :number = undefined;
-    let num2 :number = null;
-
-    let arr:number[] = [1,2,3];
-    let arr1:string[] = ["1","2","3"];
-
-    let arr3:Array<string> = ["1","2","3"];
-
-    // arr1[0].
-
-    // let arr2:any[]=["1","2"];
-
-    // arr2[0].
-
-    // 元组:数组内容的数据类型和数组的长度都限制死的数组
-    let tArr:[number,number,string,boolean]=[1,2,"3",true];
-    // tArr[4]=123
-
-
-    // 枚举的所有属性值,从0开始自增长
-    enum City{
-        "beijing"=100,
-        "shanghai"=10000,
-        "shenzhen",
-        "quanzhou"=9000
+    // 接口就是约束对象,可以对某些对象提出要求
+    interface IObj{
+        readonly id:number
+        name:string
+        age:number
+        sex?:string
     }
-    let obj= {
+
+    let obj :IObj = {
+        id:1,
         name:"xiaoming",
-        city:City["quanzhou"]
+        age:18,
+        sex:"nan"
     }
-    // console.log(obj,City[obj.city])
-    console.log(City)
+    // obj.id=666;
 
-    let a:any=1;
-    a="str";
-    a=true;
+    console.log(obj)
 
-    // void代表没有任何数据
-    // let v1:void = undefined;
+    // const obj1= {name:123}
+    // obj1.name=666;
 
-    let obj1:object={
-        name:"xiaohong"
+
+    interface IFn{
+        (a:number,b:number,c:number):number
     }
 
-    let b:number|string=123;
+    let fn:IFn = function(a:number,b:number):number{
+        return a+b
+    }
+    fn(1,2,3)
+
+    // let fn1 = function(a:number,b:number):void{
+    // }
+    // fn1(1,2)
 
 
-    // 类型断言->告诉TS你信我
-    function getLength(value:number|string){
-        if((value as string).length){
-            return (<string>value).length
-        }else{
-            return value
+
+})();
+
+(function(){
+
+    interface IPerson{
+        name:string
+        sayHello:(text:string)=>void
+    }
+
+    interface IStudent extends IPerson{
+        price:number
+    }
+
+    class Student implements IStudent{
+        name:string;
+        price:number;
+        sayHello(text:string){
+            console.log(text)
         }
     }
 
-    let aabb;
-    aabb = "123";
-    aabb=123;
 })();
