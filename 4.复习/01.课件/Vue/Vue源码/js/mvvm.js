366035cafb60aba6bf4fe4ee6d3b48e022fdb6e2
HotJuly <949588380@qq.com>
2021-10-12 11:06:40 +0800

    16.MVVM源码分析(数据代理和数据劫持)

--- 4.复习/01.课件/Vue/Vue源码/js/mvvm.js
+++ 4.复习/01.课件/Vue/Vue源码/js/mvvm.js
@@ -1,19 +1,63 @@
 function MVVM(options) {
+  /*
+    options={
+        el: "#app",
+        data: {
+          msg: "hello mvvm",
+          person:{
+            name:"xiaoming",
+            msg:"123"
+          },
+        }
+    }
+    this->vm对象(MVVM的实例对象)
+  */
    //把配置对象保存给vm一份
     this.$options = options;
     //把配置对象的data保存到vm的_data 地址一样
     var data = this._data = this.$options.data;
+    
+    // var data = (this._data = this.$options.data);
 
     var me = this; //保存vm给me
 
+    /*
+      响应式:当某些属性的属性值发生变化之后,会更新页面
+      拆解需求:
+          1.如何知道属性值发生变化?
+            监视用户对属性值的修改?
+            可以通过Object.defineProperty来设置属性的get和set方法,从而监视用户的属性值的读取和修改
+          2.如何更新页面?
+
+      1.数据代理
+      目的:主要是为了减少书写的代码量,方便开发者书写代码,与响应式无关(读取或者设置属性都不需要this.$data了)
+      注意:
+        只有data对象的直系属性名才会被数据代理
+    */
     Object.keys(data).forEach(function(key) {
       // 遍历data当中的所有属性，每个属性都会调用下面的_proxy方法
         me._proxy(key);
     });
 
+    
+    // ["msg","person"].forEach(function(key) {
+    //   // 遍历data当中的所有属性，每个属性都会调用下面的_proxy方法
+    //     vm._proxy("msg");
+    // });
+
+    /*
+      2.数据劫持
+      目的:主要是为了监视用户对data数据的修改
+      间接目的:通知视图更新
+      注意:
+        data对象中具有多少个直接或者间接属性,都会被劫持
+    */
+
     observe(data, this);
 
-
+    /*
+      3.模版解析
+    */
     this.$compile = new Compile(options.el || document.body, this)
 }
 
@@ -23,6 +67,7 @@ MVVM.prototype = {
     },
 
     _proxy: function(key) {
+      //     vm._proxy("msg");
       // key=>"msg" this=>vm对象
       // 保存vm以方便下面的函数中也可以找到vm
         var me = this;
@@ -41,6 +86,21 @@ MVVM.prototype = {
                 me._data[key] = newVal;
             }
         });
+
+        obj.name=123
+        console.log(obj.name)
+      //   Object.defineProperty(vm, "msg", {
+      //     configurable: false, //不能重复定义
+      //     enumerable: true, //可以遍历
+      //     get: function proxyGetter() {
+      //       //当访问vm身上的属性值的时候，会去返回data的同名属性值
+      //         return vm._data["msg"];
+      //     },
+      //     set: function proxySetter(newVal) {
+      //       //当设置vm身上的属性值的时候，会去设置data的同名属性值
+      //         vm._data["msg"] = newVal;
+      //     }
+      // });
       }
 
 };
--- 4.复习/01.课件/Vue/Vue源码/js/observer.js
+++ 4.复习/01.课件/Vue/Vue源码/js/observer.js
@@ -1,23 +1,41 @@
 function Observer(data) {
+    // data就是$options.data,vm._data
+    // this->ob对象
     this.data = data;
     this.walk(data);//走起
 }
 
 Observer.prototype = {
     walk: function(data) {
+        // data就是$options.data,vm._data
+        // this->ob对象
         var me = this; //保存Observer实例化对象，因为下面要用
         Object.keys(data).forEach(function(key) {
             me.convert(key, data[key]);
         });
+        // ["msg","person"].forEach(function(key) {
+        //     ob.convert("msg", "hello mvvm");
+        // });
+        
+        // ["name","msg"].forEach(function(key) {
+        //     ob.convert("name", "xiaoming");
+        // });
     },
     convert: function(key, val) { 
+        //     ob.convert("msg", "hello mvvm");
         this.defineReactive(this.data, key, val);  
+        // this.defineReactive(vm._data, "msg", "hello mvvm");  
     },
 
     defineReactive: function(data, key, val) { 
+        // vm._data, "msg", "hello mvvm"
+
+        // 目前看来,data中每有一个直系属性就会生成一个Dep的实例对象
         var dep = new Dep();
 
+        // 如果属性值是一个对象数据类型,对当前这个对象进行深度递归劫持
         var childObj = observe(val); 
+        // var childObj = observe("hello mvvm"); 
         
         Object.defineProperty(data, key, {
             enumerable: true, // 可枚举
@@ -40,11 +58,43 @@ Observer.prototype = {
             }
         });
 
+
+        // value值和get/set方法冲突,两者只能存在一个
+        // 数据劫持虽然重写了所有的属性,但是通过闭包的方式保留了当前属性的属性值
+        // Object.defineProperty(data, "msg", {
+        //     enumerable: true, // 可枚举
+        //     configurable: false, // 不能再define
+
+        //     get: function() {
+              
+        //         if (Dep.target) {
+        //             dep.depend();
+        //         }
+        //         return val;
+        //     },
+        //     set: function(newVal) {
+            //         如果当前的值和之前的旧值相同,不会触发视图更新
+        //         if (newVal === val) {
+        //             return;
+        //         }
+        //         val = newVal;
+
+        /*
+            响应式属性的创建时机:
+                1.组件在初始化的时候,就会将data中的所有属性都进行数据劫持,变成响应式属性
+                2.当修改一个响应式属性的属性值的时候,如果传入的新值是一个对象,那么会进行深度数据劫持
+        */
+        //         childObj = observe(newVal);
+        //         dep.notify();
+        //     }
+        // });
+
     }
 };
 
 
 function observe(value, vm) {
+    // value=>data对象
     if (!value || typeof value !== 'object') {
         return;
     }
