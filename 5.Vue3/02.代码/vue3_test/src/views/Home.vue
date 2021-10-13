40edeceb3ee9f2b693424f20a1764398c7748d87
HotJuly <949588380@qq.com>
2021-10-13 17:14:55 +0800

    11.ref使用

--- 5.Vue3/02.代码/vue3_test/src/views/Home.vue
+++ 5.Vue3/02.代码/vue3_test/src/views/Home.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="home">
-    home
+    <input ref="input11111" type="text">
   </div>
 </template>
 
 <script>
+import {onMounted, ref} from 'vue';
 export default {
   name: 'Home',
-  data(){
+  setup(){
+    const input11111 = ref(null);
+    onMounted(()=>{
+      console.log(input11111)
+      input11111.value.focus();
+    })
     return{
-      msg:123,
+      input11111
     }
   },
-  props:["a"],
-  setup(props, context){
-
-    console.log(props.a, context)
-    return{
-    }
-  },
+  // mounted(){
+  //   this.$refs.input111.focus();
+  // }
 }
 </script>
